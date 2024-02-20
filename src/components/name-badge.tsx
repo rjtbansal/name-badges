d1a564f40ff480a31457dfb4a7696483a2778fe3
Rajat Bansal <rjtbansal@gmail.com>
2024-02-20 07:33:55 -0500

    Lesson/components (#1)
    name-badge and controls component were migrated to typescript

--- src/components/name-badge.tsx
+++ src/components/name-badge.tsx
@@ -1,12 +1,18 @@
-const NameBadge = () => {
+type NameBadgeProps = {
+  name: string;
+  // ? means greeting is optional. Hover over line 4 on VS Code and it shows type as string or undefined
+  greeting?: string;
+};
+
+const NameBadge = ({ name, greeting }: NameBadgeProps) => {
   return (
     <section className="badge">
       <header className="badge-header">
-        <h1 className="text-5xl">HELLO</h1>
+        <h1 className="text-5xl">{greeting}</h1>
         <p>My name is…</p>
       </header>
       <div className="badge-body">
-        <p className="badge-name">Steve</p>
+        <p className="badge-name">{name}</p>
       </div>
       <footer className="badge-footer" />
     </section>
